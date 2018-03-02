fb33da2ae2e3beb0476b5ea8436817578450cc74
Dmitry <kdp19991@gmail.com>
2018-03-02 22:41:08 +0200

    add db connection

---
+++ db.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+
+let db_uri;
+if (process.env.NODE_ENV === 'production') {
+    db_uri = process.env.MONGO_URI;
+} else {
+    db_uri = config.resourse_server.db_uri;
+}
+
+mongoose.connect(db_uri); 
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + db_uri);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
+});

a63da471bcbdad5010d6712a7857c57c0f24cce7
Dmitry <kdp19991@gmail.com>
2018-03-02 21:25:56 +0200

    add admin permissions

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    port: 3000
+    port: 3000,
+    admin_permissions: {
+        ACCESS_VERIFICATION_PASSWORD: 1,
+        ACCESS_VERIFICATION_ACCOUNT: 2,
+        ACCESS_CREATE_GROUPS: 4,
+        ACCESS_DELETE_ACCOUNTS: 8,
+        ACCESS_ADMIN: 16
+    }
 }

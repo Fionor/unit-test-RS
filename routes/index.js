943c1c994f3947fb3d977e33f29a150aa6371d28
Dmitry <kdp1999@yandex.ru>
2018-03-04 11:08:49 +0200

    add /users.create route

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ctrl_users = require('../controllers/users');
 
 router.get('/users.get', ctrl_users.get);
+router.post('/users.create', ctrl_users.create);
 
 module.exports = router;

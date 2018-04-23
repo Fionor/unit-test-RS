e5e8a50bd412d478f02987dd90d84919ddd1dc8a
Dmitry <kdp1999@yandex.ru>
2018-04-23 10:14:55 +0300

    add students routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const upload = multer();
 
 const ctrl_users = require('../controllers/users/');
+const ctrl_students = require('../controllers/students');
 const ctrl_tests = require('../controllers/tests/');
 const ctrl_photos = require('../controllers/photos/');
 
@@ -15,6 +16,9 @@ router.post('/tests.create', ctrl_tests.create);
 
 router.get('/photos.get', ctrl_photos.get);
 router.post('/photos.create', upload.fields([{name: 'photo'}]), ctrl_photos.create);
+
+router.post('/students.begin_testing', ctrl_students.begin_testing);
+router.post('/students.set_answer', ctrl_students.set_answer);
 
 
 module.exports = router;

479aea534a4dfa1c1b6dfa9f18b86ebbec68fe4c
Dmitry <kdp1999@yandex.ru>
2018-06-03 15:29:19 +0300

    fix push port

--- controllers/students/versions/v1.js
+++ controllers/students/versions/v1.js
@@ -95,7 +95,7 @@ module.exports.set_answer = async (req, res) => {
         get_user_statistic(student.user_id, test._id).then((statistic) => {
             request({
                 method: 'POST',
-                url: `http://${config.push_service.url}${config.push_service.port ? `:${config.resourse_server.port}` : ''}/push.send`,
+                url: `http://${config.push_service.url}${config.push_service.port ? `:${config.push_service.port}` : ''}/push.send`,
                 json: {statistic, room: test._id}
             })
         });

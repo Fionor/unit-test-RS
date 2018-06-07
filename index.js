c3201a9e0afd858d5249f1dcff316936fe9e7d67
Dmitry <kdp1999@yandex.ru>
2018-06-07 23:16:03 +0300

    Add api limiter

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('./models/teachers');
 require('./models/tests');
 require('./models/users');
 require('./models/photos');
-
+const rate_limit = require('express-rate-limit');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -25,14 +25,18 @@ const config = require('./config');
 const routes = require('./routes/index');
 
 app.set('port', process.env.PORT || config.resourse_server.port);
-
+const api_limiter = new rate_limit({
+    windowMs: 5*60*1000, // 5 minutes
+    max: 100,
+    delayMs: 0 // disabled
+  });
 app.use(logger('dev'));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', cors(), routes);
+app.use('/', api_limiter, cors(), routes);
 
 app.use(function(req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
